521e511b52f9c804c74a08500e647058496dd37c
Ryan Maas <ryanmaas@Ryans-MacBook-Pro.local>
2019-11-05 21:39:47 -0800

    Commit this year

---
+++ starter-code/src/main/java/edu/uw/cs/Query.java
@@ -0,0 +1,311 @@
+package edu.uw.cs;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import javax.xml.bind.*;
+
+/**
+ * Runs queries against a back-end database
+ */
+public class Query {
+  // DB Connection
+  private Connection conn;
+
+  // Password hashing parameter constants
+  private static final int HASH_STRENGTH = 65536;
+  private static final int KEY_LENGTH = 128;
+
+  // Canned queries
+  private static final String CHECK_FLIGHT_CAPACITY = "SELECT capacity FROM Flights WHERE fid = ?";
+  private PreparedStatement checkFlightCapacityStatement;
+  // TODO: YOUR CODE HERE
+
+  /**
+   * Establishes a new application-to-database connection. Uses the
+   * dbconn.properties configuration settings
+   * 
+   * @throws IOException
+   * @throws SQLException
+   */
+  public void openConnection() throws IOException, SQLException {
+    // Connect to the database with the provided connection configuration
+    Properties configProps = new Properties();
+    configProps.load(new FileInputStream("dbconn.properties"));
+    String serverURL = configProps.getProperty("hw1.server_url");
+    String dbName = configProps.getProperty("hw1.database_name");
+    String adminName = configProps.getProperty("hw1.username");
+    String password = configProps.getProperty("hw1.password");
+    String connectionUrl = String.format("jdbc:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s", serverURL,
+        dbName, adminName, password);
+    conn = DriverManager.getConnection(connectionUrl);
+
+    // By default, automatically commit after each statement
+    conn.setAutoCommit(true);
+
+    // By default, set the transaction isolation level to serializable
+    conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+  }
+
+  /**
+   * Closes the application-to-database connection
+   */
+  public void closeConnection() throws SQLException {
+    conn.close();
+  }
+
+  /**
+   * Clear the data in any custom tables created.
+   * 
+   * WARNING! Do not drop any tables and do not clear the flights table.
+   */
+  public void clearTables() {
+    try {
+      // TODO: YOUR CODE HERE
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /*
+   * prepare all the SQL statements in this method.
+   */
+  public void prepareStatements() throws SQLException {
+    checkFlightCapacityStatement = conn.prepareStatement(CHECK_FLIGHT_CAPACITY);
+    // TODO: YOUR CODE HERE
+  }
+
+  /**
+   * Takes a user's username and password and attempts to log the user in.
+   *
+   * @param username user's username
+   * @param password user's password
+   *
+   * @return If someone has already logged in, then return "User already logged
+   *         in\n" For all other errors, return "Login failed\n". Otherwise,
+   *         return "Logged in as [username]\n".
+   */
+  public String transaction_login(String username, String password) {
+    // TODO: YOUR CODE HERE
+    return "Login failed\n";
+  }
+
+  /**
+   * Implement the create user function.
+   *
+   * @param username   new user's username. User names are unique the system.
+   * @param password   new user's password.
+   * @param initAmount initial amount to deposit into the user's account, should
+   *                   be >= 0 (failure otherwise).
+   *
+   * @return either "Created user {@code username}\n" or "Failed to create user\n"
+   *         if failed.
+   */
+  public String transaction_createCustomer(String username, String password, int initAmount) {
+    // TODO: YOUR CODE HERE
+    return "Failed to create user\n";
+  }
+
+  /**
+   * Implement the search function.
+   *
+   * Searches for flights from the given origin city to the given destination
+   * city, on the given day of the month. If {@code directFlight} is true, it only
+   * searches for direct flights, otherwise is searches for direct flights and
+   * flights with two "hops." Only searches for up to the number of itineraries
+   * given by {@code numberOfItineraries}.
+   *
+   * The results are sorted based on total flight time.
+   *
+   * @param originCity
+   * @param destinationCity
+   * @param directFlight        if true, then only search for direct flights,
+   *                            otherwise include indirect flights as well
+   * @param dayOfMonth
+   * @param numberOfItineraries number of itineraries to return
+   *
+   * @return If no itineraries were found, return "No flights match your
+   *         selection\n". If an error occurs, then return "Failed to search\n".
+   *
+   *         Otherwise, the sorted itineraries printed in the following format:
+   *
+   *         Itinerary [itinerary number]: [number of flights] flight(s), [total
+   *         flight time] minutes\n [first flight in itinerary]\n ... [last flight
+   *         in itinerary]\n
+   *
+   *         Each flight should be printed using the same format as in the
+   *         {@code Flight} class. Itinerary numbers in each search should always
+   *         start from 0 and increase by 1.
+   *
+   * @see Flight#toString()
+   */
+  public String transaction_search(String originCity, String destinationCity, boolean directFlight, int dayOfMonth,
+      int numberOfItineraries) {
+    // WARNING the below code is unsafe and only handles searches for direct flights
+    // You can use the below code as a starting reference point or you can get rid
+    // of it all and replace it with your own implementation.
+    //
+    // TODO: YOUR CODE HERE
+
+    StringBuffer sb = new StringBuffer();
+
+    try {
+      // one hop itineraries
+      String unsafeSearchSQL = "SELECT TOP (" + numberOfItineraries
+          + ") day_of_month,carrier_id,flight_num,origin_city,dest_city,actual_time,capacity,price " + "FROM Flights "
+          + "WHERE origin_city = \'" + originCity + "\' AND dest_city = \'" + destinationCity
+          + "\' AND day_of_month =  " + dayOfMonth + " " + "ORDER BY actual_time ASC";
+
+      Statement searchStatement = conn.createStatement();
+      ResultSet oneHopResults = searchStatement.executeQuery(unsafeSearchSQL);
+
+      while (oneHopResults.next()) {
+        int result_dayOfMonth = oneHopResults.getInt("day_of_month");
+        String result_carrierId = oneHopResults.getString("carrier_id");
+        String result_flightNum = oneHopResults.getString("flight_num");
+        String result_originCity = oneHopResults.getString("origin_city");
+        String result_destCity = oneHopResults.getString("dest_city");
+        int result_time = oneHopResults.getInt("actual_time");
+        int result_capacity = oneHopResults.getInt("capacity");
+        int result_price = oneHopResults.getInt("price");
+
+        sb.append("Day: " + result_dayOfMonth + " Carrier: " + result_carrierId + " Number: " + result_flightNum
+            + " Origin: " + result_originCity + " Destination: " + result_destCity + " Duration: " + result_time
+            + " Capacity: " + result_capacity + " Price: " + result_price + "\n");
+      }
+      oneHopResults.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * Implements the book itinerary function.
+   *
+   * @param itineraryId ID of the itinerary to book. This must be one that is
+   *                    returned by search in the current session.
+   *
+   * @return If the user is not logged in, then return "Cannot book reservations,
+   *         not logged in\n". If try to book an itinerary with invalid ID, then
+   *         return "No such itinerary {@code itineraryId}\n". If the user already
+   *         has a reservation on the same day as the one that they are trying to
+   *         book now, then return "You cannot book two flights in the same
+   *         day\n". For all other errors, return "Booking failed\n".
+   *
+   *         And if booking succeeded, return "Booked flight(s), reservation ID:
+   *         [reservationId]\n" where reservationId is a unique number in the
+   *         reservation system that starts from 1 and increments by 1 each time a
+   *         successful reservation is made by any user in the system.
+   */
+  public String transaction_book(int itineraryId) {
+    // TODO: YOUR CODE HERE
+    return "Booking failed\n";
+  }
+
+  /**
+   * Implements the pay function.
+   *
+   * @param reservationId the reservation to pay for.
+   *
+   * @return If no user has logged in, then return "Cannot pay, not logged in\n"
+   *         If the reservation is not found / not under the logged in user's
+   *         name, then return "Cannot find unpaid reservation [reservationId]
+   *         under user: [username]\n" If the user does not have enough money in
+   *         their account, then return "User has only [balance] in account but
+   *         itinerary costs [cost]\n" For all other errors, return "Failed to pay
+   *         for reservation [reservationId]\n"
+   *
+   *         If successful, return "Paid reservation: [reservationId] remaining
+   *         balance: [balance]\n" where [balance] is the remaining balance in the
+   *         user's account.
+   */
+  public String transaction_pay(int reservationId) {
+    // TODO: YOUR CODE HERE
+    return "Failed to pay for reservation " + reservationId + "\n";
+  }
+
+  /**
+   * Implements the reservations function.
+   *
+   * @return If no user has logged in, then return "Cannot view reservations, not
+   *         logged in\n" If the user has no reservations, then return "No
+   *         reservations found\n" For all other errors, return "Failed to
+   *         retrieve reservations\n"
+   *
+   *         Otherwise return the reservations in the following format:
+   *
+   *         Reservation [reservation ID] paid: [true or false]:\n" [flight 1
+   *         under the reservation] [flight 2 under the reservation] Reservation
+   *         [reservation ID] paid: [true or false]:\n" [flight 1 under the
+   *         reservation] [flight 2 under the reservation] ...
+   *
+   *         Each flight should be printed using the same format as in the
+   *         {@code Flight} class.
+   *
+   * @see Flight#toString()
+   */
+  public String transaction_reservations() {
+    // TODO: YOUR CODE HERE
+    return "Failed to retrieve reservations\n";
+  }
+
+  /**
+   * Implements the cancel operation.
+   *
+   * @param reservationId the reservation ID to cancel
+   *
+   * @return If no user has logged in, then return "Cannot cancel reservations,
+   *         not logged in\n" For all other errors, return "Failed to cancel
+   *         reservation [reservationId]\n"
+   *
+   *         If successful, return "Canceled reservation [reservationId]\n"
+   *
+   *         Even though a reservation has been canceled, its ID should not be
+   *         reused by the system.
+   */
+  public String transaction_cancel(int reservationId) {
+    // TODO: YOUR CODE HERE
+    return "Failed to cancel reservation " + reservationId + "\n";
+  }
+
+  /**
+   * Example utility function that uses prepared statements
+   */
+  private int checkFlightCapacity(int fid) throws SQLException {
+    checkFlightCapacityStatement.clearParameters();
+    checkFlightCapacityStatement.setInt(1, fid);
+    ResultSet results = checkFlightCapacityStatement.executeQuery();
+    results.next();
+    int capacity = results.getInt("capacity");
+    results.close();
+
+    return capacity;
+  }
+
+  /**
+   * A class to store flight information.
+   */
+  class Flight {
+    public int fid;
+    public int dayOfMonth;
+    public String carrierId;
+    public String flightNum;
+    public String originCity;
+    public String destCity;
+    public int time;
+    public int capacity;
+    public int price;
+
+    @Override
+    public String toString() {
+      return "ID: " + fid + " Day: " + dayOfMonth + " Carrier: " + carrierId + " Number: " + flightNum + " Origin: "
+          + originCity + " Dest: " + destCity + " Duration: " + time + " Capacity: " + capacity + " Price: " + price;
+    }
+  }
+}
